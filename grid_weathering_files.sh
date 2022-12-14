71da469a88520b05590e2ce08503e52619bfdf3d
flacroix-ubern <119116449+flacroix-ubern@users.noreply.github.com>
2022-12-14 19:59:21 +0100

    Update description

--- grid_weathering_files.sh
+++ grid_weathering_files.sh
@@ -9,7 +9,7 @@ ECHAM_AREA="echam_inputs/echam_area.nc"
 #Run weathering model
 python weathering.py $ECHAM_FILE $ECHAM_AREA
 
-# Regrid P and Alk release from ECHAM to HD model grid for calculations of transformations
+# Add grid and unit information
 cdo setunit,"g P m-2 yr-1" -setgrid,$ECHAM_FILE test_prelease.nc prelease_wthgrd.nc
 cdo setunit,"g C m-2 yr-1 of HCO3-" -setgrid,$ECHAM_FILE test_alkrelease.nc alkrelease_wthgrd.nc
 cdo setunit,"g C m-2 yr-1 of CO2" -setgrid,$ECHAM_FILE test_co2drawdown.nc co2drawdown_wthgrd.nc
